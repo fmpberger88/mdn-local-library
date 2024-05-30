00a75314f3605c0c95a272f7ef55ea29d61161c8
fmpberger88 <fabian.berger@protonmail.ch>
2024-05-30 19:27:13 +0200

    implement detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,5 +1,7 @@
 const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
+const Book = require("../models/book");
+const res = require("express/lib/response");
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -15,7 +17,23 @@ exports.genre_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+    // Get details of genre by name and all associated books
+    const genre = await Genre.findOne({ name: req.params.id }).exec();
+
+    // No results
+    if (genre === null) {
+        const err = new Error("Genre not found");
+        err.status = 400;
+        return next(err);
+    }
+
+    const booksInGenre = await Book.find({ genre: genre._id }, "title summary").exec();
+
+    res.render('genre_detail', {
+        title: "Genre Detail",
+        genre: genre,
+        genre_books: booksInGenre
+    });
 });
 
 // Display Genre create form on GET.
