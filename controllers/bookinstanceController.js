2e754f59b3419b9ae19db0046b8760695d13f21d
fmpberger88 <fabian.berger@protonmail.ch>
2024-05-30 22:30:39 +0200

    implement bookinstance detail

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -15,7 +15,20 @@ exports.bookinstance_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+    const bookInstance = await BookInstance.findById(req.params.id)
+        .populate("book")
+        .exec()
+
+    if (bookInstance === null) {
+        const err = new Error("Book copy not found");
+        err.status = 400;
+        return next(err);
+    }
+
+    res.render('bookinstance_detail', {
+        title: "Book Instance Detail",
+        bookinstance: bookInstance,
+    });
 });
 
 // Display BookInstance create form on GET.
