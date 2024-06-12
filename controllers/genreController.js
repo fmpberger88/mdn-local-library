690c1a215ce16bb04d0bf22f1dd7a60fe8f2c72c
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-12 17:59:12 +0200

    update routes for genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -128,10 +128,39 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Genre update GET");
+    const genre = await Genre.findById(req.params.id).exec();
+
+    res.render('genre_form', {
+        title: "Update Genre",
+        genre: genre
+    })
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+    body('name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const genre = new Genre({
+            name: req.body.name,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            res.render("genre_form", {
+                title: "Update Genre",
+                genre: genre,
+                errors: errors.array(),
+            })
+            return;
+        } else {
+            const updatedGenre = await Genre.findByIdAndUpdate(req.params.id, genre, {});
+            res.redirect(updatedGenre.url);
+        }
+    })
+];
